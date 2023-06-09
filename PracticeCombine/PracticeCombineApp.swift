c6922ce9b09be304e9fa9585239ffcf730231b3b
hsuuuk <hsuuuk.ios@gmail.com>
2023-06-09 10:02:39 +0900

    Initial Commit

---
+++ PracticeCombine/PracticeCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeCombineApp.swift
+//  PracticeCombine
+//
+//  Created by 심현석 on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct PracticeCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
